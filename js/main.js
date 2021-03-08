f89bda0f8492544a7e1505f1df6e23b90e757198
Latatiana <lakttattt@gmail.com>
2021-03-08 18:41:50 +0500

    create: keyboard navigation

--- js/main.js
+++ js/main.js
@@ -11,6 +11,9 @@ const swiper = new Swiper(".swiper-container", {
   // Optional parameters
   loop: true,
   // Navigation arrows
+  keyboard: {
+    enabled: true,
+    },
   navigation: {
     nextEl: ".slider-button-next",
     prevEl: ".slider-button-prev",
