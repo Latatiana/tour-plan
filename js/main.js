02e59952f090f4733567ce74a5ce886702d65a6c
Latatiana <lakttattt@gmail.com>
2021-03-08 18:31:29 +0500

    create: slider

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// var swiper = new Swiper('.swiper-container', {
+//   // Optional parameters
+//   loop: true,
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.slider-button-next',
+//     prevEl: '.slider-button-prev',
+//   },
+// });
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-button-next",
+    prevEl: ".slider-button-prev",
+  },
+
+});
