7d697dc522ed8b27d13838b73e74c4a3923bef31
Latatiana <lakttattt@gmail.com>
2021-03-11 22:12:56 +0500

    create: Api map

--- js/main.js
+++ js/main.js
@@ -23,3 +23,67 @@ const reviewsSlider = new Swiper(".reviews-slider", {
     prevEl: '.reviews-slider__button-prev',
   },
 });
+
+//map
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+            center: [10.30803757761703, 124.01952350845728],
+            zoom: 10
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+    // Создаем геообъект с типом геометрии "Точка".
+        myGeoObject = new ymaps.GeoObject({
+            // Описание геометрии.
+            geometry: {
+                type: "Point",
+                coordinates: [10.30803757761703, 124.01952350845728]
+            },
+            // Свойства.            
+        }, {
+            // Опции.
+            // Иконка метки будет растягиваться под размер ее содержимого.
+            preset: 'islands#blackStretchyIcon',
+            // Метку можно перемещать.
+            draggable: true
+        }),
+        myPieChart = new ymaps.Placemark([
+            10.30803757761703, 124.01952350845728
+        ], {
+            // Данные для построения диаграммы.
+            data: [
+                {weight: 8, color: '#0E4779'},
+                {weight: 6, color: '#1E98FF'},
+                {weight: 4, color: '#82CDFF'}
+            ],
+            iconCaption: "Диаграмма"
+        }, {
+            // Зададим произвольный макет метки.
+            iconLayout: 'default#pieChart',
+            // Радиус диаграммы в пикселях.
+            iconPieChartRadius: 30,
+            // Радиус центральной части макета.
+            iconPieChartCoreRadius: 10,
+            // Стиль заливки центральной части.
+            iconPieChartCoreFillStyle: '#ffffff',
+            // Cтиль линий-разделителей секторов и внешней обводки диаграммы.
+            iconPieChartStrokeStyle: '#ffffff',
+            // Ширина линий-разделителей секторов и внешней обводки диаграммы.
+            iconPieChartStrokeWidth: 3,
+            // Максимальная ширина подписи метки.
+            iconPieChartCaptionMaxWidth: 200
+        });
+
+    myMap.geoObjects
+                      
+        .add(new ymaps.Placemark([10.30803757761703, 124.01952350845728], {
+            balloonContent: 'цвет <strong>носика Гены</strong>',
+            iconCaption: 'GRAND HILTON HOTEL'
+        }, {
+            preset: 'islands#greenDotIconWithCaption'
+        }))
+        
+}
