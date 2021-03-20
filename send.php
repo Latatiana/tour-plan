8242a47d75f3c87f6c3df18c8f365cc70d3c11a0
Latatiana <lakttattt@gmail.com>
2021-03-20 11:54:19 +0500

    create: send information from newsletter

--- send.php
+++ send.php
@@ -8,55 +8,58 @@ require 'phpmailer/Exception.php';
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
-$email = $_POST['email'];
-
+// вторая форма
+$sub = $_POST['sub'];
+// Формирование самого письма
+$title2 = "Подписка Best Tour Plan";
+$body2 = "
+<h2>subscribe</h2>
+<b>Имя:</b> $sub
+";
 // Формирование самого письма
 $title = "Новое обращение Best Tour Plan";
-$body = "
+$body1 = "
 <h2>Новое обращение</h2>
 <b>Имя:</b> $name<br>
 <b>Телефон:</b> $phone<br><br>
 <b>Сообщение:</b><br>$message
 ";
 
-$title = "subscribe to our NEWSLETTER Best Tour Plan";
-$body = "
-<h2>subscribe</h2>
-<b>Email:</b> $email<br>
-";
 // Настройки PHPMailer
 $mail = new PHPMailer\PHPMailer\PHPMailer();
-try {
+
     $mail->isSMTP();   
     $mail->CharSet = "UTF-8";
     $mail->SMTPAuth   = true;
-    //$mail->SMTPDebug = 2;
+    // $mail->SMTPDebug = 2;
     $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
 
     // Настройки вашей почты
     $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
-    $mail->Username   = 'lakttattt99@mail.ru'; // Логин на почте
-    $mail->Password   = 'testtest1234'; // Пароль на почте
+    $mail->Username   = 'laktionovat@internet.ru'; // Логин на почте
+    $mail->Password   = 'irmiqscjtdlserhl'; // Пароль на почте
     $mail->SMTPSecure = 'ssl';
     $mail->Port       = 465;
-    $mail->setFrom('lakttattt99@mail.ru', 'Татьяна Лактионова'); // Адрес самой почты и имя отправителя
+    $mail->setFrom('laktionovat@internet.ru', 'Татьяна Лактионова'); // Адрес самой почты и имя отправителя
 
     // Получатель письма
     $mail->addAddress('tan.laktionova@yandex.ru');  
-  
-// Отправка сообщения
-$mail->isHTML(true);
-$mail->Subject = $title;
-$mail->Body = $body;    
-
-// Проверяем отравленность сообщения
-if ($mail->send()) {$result = "success";} 
-else {$result = "error";}
-
-} catch (Exception $e) {
-    $result = "error";
-    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+ if (empty($_POST['name'] && $_POST['phone'] )&&!empty($_POST['sub'])) {
+    $mail->isHTML(true);
+    $mail->Subject = $title2;
+    $mail->Body = $body2;  
+    // Проверяем отравленность сообщения
+    if ($mail->send()) {$result = "success";} 
+    else {$result = "error";}
+    header('Location: thankyou.html');
 }
+elseif (!empty($_POST['name'] && $_POST['phone'] )&&empty($_POST['sub'])) {
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body1;    
 
-// Отображение результата
-header('Location: thankyou.html');
+    // Проверяем отравленность сообщения
+    if ($mail->send()) {$result = "success";} 
+    else {$result = "error";}
+    header('Location: thankyou.html');
+}  
